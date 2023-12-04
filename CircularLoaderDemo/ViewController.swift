8da3d76287f0a527b44a66aaa0dfcac5a18050f8
Pratham Gupta <pratham.gupta@bajajfinserv.in>
2023-12-04 10:41:44 +0530

    Minor Change

--- CircularLoaderDemo/ViewController.swift
+++ CircularLoaderDemo/ViewController.swift
@@ -8,14 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var circluarLoaderView: CircularLoaderView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         circluarLoaderView.configure(delegate: self, radius: 100, numberOfObjects: 12, objectSize: 50)
-//        circluarLoaderView.animateInfiniteCircularProgressView()
     }
+}
 
 extension ViewController: CircularLoaderViewDelegate {
     func drawObject(circluarView: CircularLoaderView, index: Int, drawPoint: CGPoint) {
